3519cdd85142a79aba081f44d79e2668d0e91a9c
0xOnyx <jeremy.dosg@gmail.comw>
2022-12-14 16:20:10 +0100

    final cpp04

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,17 @@
+//
+// Created by onyx on 12/14/22.
+//
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+class Bureaucrat {
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat &bureaucrat);
+	~Bureaucrat(void);
+	Burea
+private:
+};
+
+#endif
